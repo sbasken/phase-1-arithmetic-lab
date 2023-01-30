684dfd3f2a78159785c7c0661a0eb0829edbc713
Saki <sbasken@gmail.com>
2023-01-30 15:54:22 -0800

    pass tests

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random()*10+1)
+
+let num3 = 20;
+let num4 = 16;
+const mod = num3 % num4;
+
+const max = Math.max(1,5,6,20);
